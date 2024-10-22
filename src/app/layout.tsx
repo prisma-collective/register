398d859d51f164c7df0acfea45aa8728b70421bc
Tobias Fechner <tobias.fechner@hotmail.co.uk>
2024-10-22 00:44:56 -0400

    Add more favicon files to public folder. Add manifest.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="../public/favicon.ico" sizes="any" />
+        <link rel="icon" href="../public/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="../public/apple-touch-icon.png" />
+        <link rel="manifest" href="../public/manifest.json" />
         {/* You can add other icons for various platforms if needed */}
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
